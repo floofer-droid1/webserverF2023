e7f0d76dc97778acd14c9fb21b85b5d38ca2c3a6
Floofer-droid <70661432+Floofer-droid@users.noreply.github.com>
2023-10-04 14:14:05 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,14 +9,15 @@
             <p> what OS do you use?</p>
             <input type="radio" id="android" name="os" value="android"> <label for="android">Android</label>
             <input type="radio" id="ios" name="os" value="ios"> <label for="ios">ios</label>
-
+            
+            <p> who's you favourite phone manufacturer?</p>
+            <p><label for="manufacturer">Favourite manufacturer: </label><input type="text" id="manufacturer" name="manufacturer" /></p>
+            
             <p><label for="music">how do you listen to music?</label><br>
             <select id="music" name="music">
                 <option value="wired">wired</option>
                 <option value="wireless">Wireless</option>
             </br>
-            <p> who's you favourite phone manufacturer?</p>
-            <p><label for="manufacturer">Favourite manufacturer: </label><input type="text" id="manufacturer" name="manufacturer" /></p>
             <p><input type="submit" /></p>
         </form>
     </body>
