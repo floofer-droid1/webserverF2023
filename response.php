b39bde5a26b04858ba727ce35df9959debd51eeb
Floofer-droid <70661432+Floofer-droid@users.noreply.github.com>
2023-10-04 14:29:36 -0400

    Update response.php

--- response.php
+++ response.php
@@ -4,8 +4,8 @@
         <title>php response</title>
     </head>
     <body>
-        <p>you are a <?= htmlspecialhars ($_POST["os"])?> user </p>
-        <p>that likes to listen to their tunes the <?= htmlspecialhars ($_POST["music"])?> way </p>
-        <p>your favourite phone manufacturer is <?= htmlspecialhars ($_POST["manufacturer"])?></p>
+        <p>you are a <?= htmlspecialchars ($_POST["os"])?> user </p>
+        <p>that likes to listen to their tunes the <?= htmlspecialchars ($_POST["music"])?> way </p>
+        <p>your favourite phone manufacturer is <?= htmlspecialchars ($_POST["manufacturer"])?></p>
     </body>
 </html>
